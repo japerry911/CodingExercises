18258467b9da005a2ab673b8578e83d9512b1e29
japerry911 <jack.e.perry@outlook.com>
2020-09-15 11:03:09 -0600

    Python reverseString challenge completed

---
+++ Python/ReverseString.py
@@ -0,0 +1,19 @@
+"""
+---Reverse String---
+Write a function that reverses a string. The input string is given as an 
+array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying 
+the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+"""
+
+
+def reverse_string(s) -> None:
+    s.reverse()
+    print(s)
+
+
+if __name__ == "__main__":
+    reverse_string(["h", "e", "l", "l", "o"])

4b3c4f85232f1cc9d527032da64a8c456d58cb85
japerry911 <jack.e.perry@outlook.com>
2020-09-28 11:05:47 -0600

    Python Subtract the Product and Sum from an Integer challenege completed - added functools import

--- Python/SubtractProductSum.py
+++ Python/SubtractProductSum.py
@@ -4,6 +4,8 @@ Given an integer number n, return the difference between the
 product of its digits and the sum of its digits. 
 """
 
+from functools import reduce
+
 
 def subtract_product_and_sum(n: int) -> int:
     return reduce(lambda acc, x: acc * int(x), str(n), 1) - sum(list(map(lambda x: int(x), str(n))))

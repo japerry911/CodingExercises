9cc4334e206af4cc307558a8325c4c8dc4a90107
japerry911 <jack.e.perry@outlook.com>
2020-09-29 09:26:03 -0600

    Python XOR Operating in a List completed

---
+++ Python/XOROOperationList.py
@@ -0,0 +1,12 @@
+"""
+---XOR Operating in an Array---
+Given an integer n and an integer start.
+
+Define an array nums where nums[i] = start + 2*i (0-indexed) and n == nums.length.
+
+Return the bitwise XOR of all elements of nums.
+"""
+
+
+def xor_operation(n: int, start: int) -> int:
+    return reduce(lambda acc, x: acc ^ x, list(range(start, (n * 2) + start, 2)))
